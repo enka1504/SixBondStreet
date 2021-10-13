7eed4235377e75820629c87f0d02fbb6a7d0498e
Dean Javior <emely44daretfb@hotmail.com>
2021-10-13 14:01:29 +0000

    Update from Shopify for theme SixBondStreet-Ira/Free-shipping-and-availability

--- assets/index.js
+++ assets/index.js
@@ -14372,8 +14372,7 @@
           
                   document.querySelector(".trigger_orb").style.display="inline-block";
                 }
-var li = $(".product-form__color-swatches").find(".variant__option a");
-console.log(li);
+
 
 				if (Ut(this.container, e), !e) return $o(h("[data-add-to-cart]", this.container), !1), void(this.availability && this.availability.unload());
 				var n = function(t, e) {
