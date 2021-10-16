44ea51579383afed553359029d0fd75e029717a1
Dean Javior <emely44daretfb@hotmail.com>
2021-10-16 12:09:03 +0000

    Update from Shopify for theme SixBondStreet-Ira/Free-shipping-and-availability

--- assets/checkoutcustom.js
+++ assets/checkoutcustom.js
@@ -1,17 +1,3 @@
 
-         if (Shopify.Checkout.step === 'payment_method')
-           
-         {
-           
-           console.log("ready");
-           
-//            var checker = document.getElementById('checkme');
-var sendbtn = document.getElementById('continue_button');
-             sendbtn.disabled = true;
-// checker.onchange = function() {
-//   sendbtn.disabled = !!this.checked;
-// };
-           
-           
-         }
    
+
