e793947fbb1a015750354645c5cf5af52a637f2d
Dean Javior <emely44daretfb@hotmail.com>
2021-10-16 12:22:44 +0000

    Update from Shopify for theme SixBondStreet-Ira/Free-shipping-and-availability

--- assets/checkoutcustom.js
+++ assets/checkoutcustom.js
@@ -3,16 +3,16 @@
            
          {
 document.querySelector(".step__footer").insertAdjacentHTML("beforeBegin",
-"<label id='agreement-label'><input type='checkbox' class='input-checkbox'> Jeg har lest og godkjenner <a href='/policies/terms-of-service'>kjøpsvilkårene</a></label>");
+"<label id='agreement-label'><input type='checkbox' class='input-checkbox' id='agreement-input'> Jeg har lest og godkjenner <a href='/policies/terms-of-service'>kjøpsvilkårene</a></label>");
            
            console.log("ready");
            
-//            var checker = document.getElementById('checkme');
+           var checker = document.getElementById('agreement-input');
 var sendbtn = document.getElementById('continue_button');
              sendbtn.disabled = true;
-// checker.onchange = function() {
-//   sendbtn.disabled = !!this.checked;
-// };
+checker.onchange = function() {
+ sendbtn.disabled = false;
+};
            
            
          }
