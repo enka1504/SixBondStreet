8b4939bf8284c20a716a1e54cf9ccb9903224c2d
Dean Javior <emely44daretfb@hotmail.com>
2022-07-09 16:35:07 +0000

    Update from Shopify for theme SixBondStreet-Ira/algolia-reinstall-test
    Committed from shop: SixBondStreet

--- assets/theme.classic.js
+++ assets/theme.classic.js
@@ -8229,13 +8229,13 @@
     const input = n$1('[data-input]', node);
     const clear = n$1('[data-clear]', node);
     const resultsContainer = n$1('[data-results]', node);
-//     const focusTrap = createFocusTrap(node); // This gets replaced with a focus trapping util on `open` however
+    const focusTrap = createFocusTrap(node); // This gets replaced with a focus trapping util on `open` however
 
-//     const overlayClick = e$2(overlay, 'click', close);
-//     const clearClick = e$2(clear, 'click', reset);
-//     const keyDown = e$2(node, 'keydown', checkEscape);
-//     const inputChange = e$2(input, 'input', handleInput);
-//     const predictiveSearch = new PredictiveSearch(resultsContainer);
+    //const overlayClick = e$2(overlay, 'click', close);
+    const clearClick = e$2(clear, 'click', reset);
+    //const keyDown = e$2(node, 'keydown', checkEscape);
+    //const inputChange = e$2(input, 'input', handleInput);
+    //const predictiveSearch = new PredictiveSearch(resultsContainer);
 
     function handleInput(e) {
       let populated = e.target.value !== '';
