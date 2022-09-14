76182845edf454b90dab08a3979288d32ba38cc2
Dean Javior <emely44daretfb@hotmail.com>
2022-09-14 12:09:03 +0000

    Update from Shopify for theme SixBondStreet-Ira/dev
    Committed from shop: SixBondStreet

--- assets/theme-81854650.js
+++ assets/theme-81854650.js
@@ -8254,6 +8254,7 @@ function QuickSearch (node) {
   }
 
   function close() {
+        console.log("tur");
     i(node, classes$5.visible);
     setTimeout(() => {
       i(node, classes$5.active);
